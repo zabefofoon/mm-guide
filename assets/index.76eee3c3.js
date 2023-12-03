b3d00e0bd4969cb12a65df2489184cce494140a8
zabefofoon <37139656+zabefofoon@users.noreply.github.com>
2023-12-03 14:49:59 +0000

    Deploying to gh-pages from @ zabefofoon/mm-guide@c8aaf98fe25e4229c6a62a3c58433248190e5e34 🚀

---
+++ assets/index.76eee3c3.js
@@ -0,0 +1,1 @@
+import{u as s}from"./vue.f36acd1f.92a53d8b.js";import{G as u}from"./entry.67f5ca03.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
