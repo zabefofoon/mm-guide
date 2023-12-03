de5fa5aa5690091567c95553e55c3b6b238da54b
zabefofoon <37139656+zabefofoon@users.noreply.github.com>
2023-12-03 03:49:40 +0000

    Deploying to gh-pages from @ zabefofoon/mm-guide@29144d2a55af674858d2850abf6a114f83d6e61e 🚀

---
+++ assets/index.36f9b050.js
@@ -0,0 +1,1 @@
+import{u as s}from"./vue.f36acd1f.ae091290.js";import{G as u}from"./entry.0a952082.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
